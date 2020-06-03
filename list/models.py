3d06105ddef0fc50671e6f7bcca405d440a8fb31
Jonathan <jmedel.7@gmail.com>
2020-06-03 11:48:32 -0400

    def de homelist para name arreglado

--- list/models.py
+++ list/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class HomeList(models.Model):
     name = models.CharField(max_length=200)
 
-    def __str_(self):
+    def __str__(self):
         return self.name
 
 class Item(models.Model):
