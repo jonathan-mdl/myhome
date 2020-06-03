d76d4067918569ccb39936252a2d45114ff6fc73
Jonathan <jmedel.7@gmail.com>
2020-06-03 15:35:36 -0400

    modelos terminados y migrados

--- list/models.py
+++ list/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class HomeList(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Home(models.Model):
+    homelist = models.ForeignKey(HomeList, on_delete=models.CASCADE)
+    commentary = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.commentary
