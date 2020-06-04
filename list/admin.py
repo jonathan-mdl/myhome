6f5f47b832068ea47cc875f7665ef862cd31ab4c
Jonathan <jmedel.7@gmail.com>
2020-06-04 09:27:34 -0400

    homelist y homes en dashboard listos

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import HomeList, Home
 # Register your models here.
+
+admin.site.register(HomeList)
+admin.site.register(Home)
--- list/views.py
+++ list/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import HomeList, Home
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Norma's list</h1>")
+def index(response, name):
+    ls = HomeList.objects.get(name=name)
+    home = ls.home_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br><p>%s</p>" %(ls.name, str(home.commentary)))
 
